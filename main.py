a4e0e9e3e8b9082d9a73cc4779478005b6348adf
Vitor Henrique <vitorfonsecaxs@gmail.com>
2024-04-23 14:38:41 -0300

    add: menu principal

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Dev From Vito
+
+import time
+print("  Bem-Vindo a jogoteca do vito  \n")
+time.sleep(1)
+
+# Main Principal 
+input("""\t(Menu Principal)\n
+(Escolha um Número para entrar no jogo)
+1- Jogo da Velha pau no cu
+2- Jogo da Adivinhação fdpta
+3- Jogo da forca se mata
+4- Calculado Mortifera
+Digite sua resposta: """)
+
