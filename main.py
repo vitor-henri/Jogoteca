095a03d6942acc407e633efcb5923c43dc6a7063
Vitor Henrique <vitorfonsecaxs@gmail.com>
2024-04-23 14:51:08 -0300

    add: validando idade

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # Dev From Vito
-
+from verificação import *
 import time
+
 print("  Bem-Vindo a jogoteca do vito  \n")
 time.sleep(1)
 
 # Main Principal 
-input("""\t(Menu Principal)\n
-(Escolha um Número para entrar no jogo)
-1- Jogo da Velha pau no cu
-2- Jogo da Adivinhação fdpta
-3- Jogo da forca se mata
-4- Calculado Mortifera
-Digite sua resposta: """)
+idade_do_menino = int(input("Qual sua idade para poder iniciar a jogoteca? "))
+if idade_do_menino >= 18:
+    input("""\t(Menu Principal)\n
+    (Escolha um Número para entrar no jogo)
+    1- Jogo da Velha pau no cu
+    2- Jogo da Adivinhação fdpta
+    3- Jogo da forca se mata
+    4- Calculado Mortifera
+    Digite sua resposta: """)
+else:
+    print("Sai daq mlk do kraio o bglh é pra de maior")
+    exit
 
